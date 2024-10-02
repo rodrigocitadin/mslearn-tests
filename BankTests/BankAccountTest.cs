fd0563d67ae6e83d78a28893fdcffd84855f3ea5
rodrigocitadin <citadinrodrigo@gmail.com>
2024-10-02 14:14:30 -0300

    first test

--- BankTests/BankAccountTest.cs
+++ BankTests/BankAccountTest.cs
@@ -1,10 +1,24 @@
 namespace BankTests;
 
+using BankAccountNS;
+
 [TestClass]
-public class UnitTest1
+public class BankAccountTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Debit_WithValidAmount_UpdatesBalance()
     {
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = 4.55;
+        double expected = 7.44;
+        BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+        // Act
+        account.Debit(debitAmount);
+
+        // Assert
+        double actual = account.Balance;
+        Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
     }
 }
