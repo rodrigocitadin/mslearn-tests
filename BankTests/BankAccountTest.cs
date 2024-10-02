93db4b2367bbb842ff34ca3a8d866fc9d92b7c52
rodrigocitadin <citadinrodrigo@gmail.com>
2024-10-02 14:19:56 -0300

    wip

--- BankTests/BankAccountTest.cs
+++ BankTests/BankAccountTest.cs
@@ -33,4 +33,24 @@ public class BankAccountTests
         // Act and assert
         Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
     }
+
+    [TestMethod]
+    public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+    {
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = 20.0;
+        BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+        // Act
+        try
+        {
+            account.Debit(debitAmount);
+        }
+        catch (System.ArgumentOutOfRangeException e)
+        {
+            // Assert
+            StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+        }
+    }
 }
